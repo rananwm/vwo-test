5c6c7f0a3b03dd92fc51ca6a83c665ad449ba0b5
rananwm <rana.nwm@gmail.com>
2024-08-08 16:21:23 +0500

    pages route

---
+++ src/pages/_document.js
@@ -0,0 +1,11 @@
+import { Html, Main } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en" data-theme="dark">
+      <body>
+        <Main />
+      </body>
+    </Html>
+  );
+}
